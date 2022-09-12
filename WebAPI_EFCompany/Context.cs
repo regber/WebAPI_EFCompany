4950b2481edd8cb13482d21bd4c2c587104c8d0f
Regber <regber.888@gmail.com>
2022-09-12 14:57:08 +0300

    Добавлен контекст БД с SQLite

--- WebAPI_EFCompany/Context.cs
+++ WebAPI_EFCompany/Context.cs
@@ -2,10 +2,25 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Sqlite;
 
 namespace WebAPI_EFCompany
 {
-    public class Context
+    public class Context:DbContext
     {
+        public Context()
+        {
+            Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data source=Company.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
     }
 }
